25269cfbd7a99509ee3f7422f1a2f3a24362ea3c
Brian D. Baker <brian.devs.baker@gmail.com>
2022-01-15 14:02:09 -0500

    Starter code for ViewOwnItem

---
+++ client/src/pages/ViewItem/ViewOwnItem/ImageUploadControls.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import {
+  FormControl,
+  FormLabel,
+  Box,
+  useToast,
+  IconButton,
+  Image,
+  ButtonGroup,
+  Spinner,
+  Center,
+} from "@chakra-ui/react";
+import { useState } from 'react';
+import FileUploadButton from '../../../components/FileUploadButton';
+import { FaTrash } from 'react-icons/fa';
+import { useMutation, useQuery } from '@apollo/client';
+import { ADD_TEMP_IMAGE, REMOVE_TEMP_IMAGE } from '../../../utils/mutations';
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from '../../../utils/firebase';
+import { DEFAULT_IMAGE_LOCATION, generateFileName, getDownloadURLForImageFile, validateImageType } from '../../../utils/imageUploads';
+import { GET_TEMP_IMAGE_FILE } from '../../../utils/queries';
+
+
+
+const ImageUploadControls = ({ formState, setFormState }) => {
+  const [addTempImage, { loading: addTempImageLoading }] = useMutation(ADD_TEMP_IMAGE);
+  const [removeTempImage, { loading: removeTempImageLoading }] = useMutation(REMOVE_TEMP_IMAGE);
+  const [isUploading, setIsUploading] = useState(false);
+  const [imageURL, setImageURL] = useState(DEFAULT_IMAGE_LOCATION);
+  const { loading: getTempImageLoading, data: getTempImageData } = useQuery(GET_TEMP_IMAGE_FILE);
+  const toast = useToast();
+
+  if (getTempImageData?.user.tempImageFile && !(imageURL.includes(getTempImageData.user.tempImageFile))) {
+    getDownloadURLForImageFile( getTempImageData.user.tempImageFile)
+      .then(url => {
+        setFormState({ ...formState, imageUploaded: true })
+        setImageURL(url);
+      });
+  }
+
+
+  const fileUploadHandler = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    try {
+      // upload to firebase
+      validateImageType(file);
+      const uploadFileName = generateFileName(file);
+      const imageRef = ref(storage, uploadFileName);
+      setIsUploading(true);
+      uploadBytes(imageRef, file)
+        .then(snapshot => getDownloadURL(snapshot.ref))
+        .then(downloadURL => {
+          addTempImage({ variables: { filename: uploadFileName } });
+          setIsUploading(false);
+          setImageURL(downloadURL);
+          setFormState({ ...formState, imageUploaded: true });
+        })
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: "The file you selected wasn't a valid image file!",
+        status: "error",
+        duration: 3000,
+        isClosable: true
+      });
+    }
+  }
+
+  const discardImageHandler = () => {
+    removeTempImage();
+    setImageURL(DEFAULT_IMAGE_LOCATION);
+    setFormState({ ...formState, imageUploaded: false });
+  };
+
+  return (
+    <Box>
+      <FormControl mb={1}>
+        <FormLabel htmlFor="fileUpload">Picture</FormLabel>
+        <ButtonGroup>
+          <FileUploadButton
+            placeholder="Click here"
+            acceptedFileTypes=".jpg, .jpeg, .png, .gif, .bmp"
+            name="fileUpload"
+            onChange={fileUploadHandler}
+            isLoading={isUploading || addTempImageLoading}
+          >Click to Upload</FileUploadButton>
+
+          <IconButton aria-label='Discard Image' isLoading={removeTempImageLoading} icon={<FaTrash />} isDisabled={imageURL === DEFAULT_IMAGE_LOCATION} onClick={discardImageHandler} />
+        </ButtonGroup>
+      </FormControl>
+      {
+        (isUploading || addTempImageLoading || removeTempImageLoading || getTempImageLoading)
+          ?
+          <Center w="320px" h="512px" border={1} boxShadow="md">
+            <Spinner size='xl' />
+          </Center>
+          :
+          <Image maxH="512px" border={1} boxShadow="md" src={imageURL} />
+      }
+
+    </Box>
+  );
+
+}
+
+export default ImageUploadControls;
---
+++ client/src/pages/ViewItem/ViewOwnItem/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { VStack, Heading, Button, useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import Auth from '../../../utils/auth';
+import { Redirect } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { ADD_ARTICLE } from '../../../utils/mutations';
+import CategoryControl from './CategoryControl';
+import DescriptionControl from './DescriptionControl';
+import TagForm from './TagForm';
+import ImageUploadControls from './ImageUploadControls';
+
+
+const ViewOwnItem = ({article}) => {
+  const [formState, setFormState] = useState(
+    {
+      description: article.description,
+      category: article.category,
+      tags: [...article.tags],
+      imageUploaded: false
+    }
+  );
+  const [submitArticle, { loading: submittedArticleLoading }] = useMutation(ADD_ARTICLE, { onCompleted: () => { return <Redirect to="/" /> } });
+  const toast = useToast();
+
+  // TODO fix authentication
+  if (!Auth.loggedIn()) {
+    // return <Redirect to="/" />;
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!formState.imageUploaded || !formState.description.trim()) {
+      toast({
+        title: 'Cannot Add Item',
+        description: "You need to upload an image or enter a description.",
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    submitArticle({
+      variables: {
+        ...formState
+      }
+    });
+  };
+
+  return (
+    <VStack padding={4} spacing={6} align="stretch" mb={10}>
+      <Heading>Add Item to Your Wardrobe</Heading>
+
+      <CategoryControl setFormState={setFormState} formState={formState} />
+
+      <ImageUploadControls setFormState={setFormState} formState={formState} />
+
+      <DescriptionControl setFormState={setFormState} formState={formState} />
+
+      <TagForm formState={formState} setFormState={setFormState} />
+
+      <Button my={8} onClick={handleSubmit} isLoading={submittedArticleLoading}>
+        Register New Item
+      </Button>
+    </VStack>
+  );
+};
+
+export default ViewOwnItem;
