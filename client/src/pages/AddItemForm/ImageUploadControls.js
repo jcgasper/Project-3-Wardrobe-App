3cff5f975dbeea08fd80d52e5d34665d42151381
Brian D. Baker <brian.devs.baker@gmail.com>
2022-01-14 21:07:39 -0500

    Added useQuery to check for image file on load

--- client/src/pages/AddItemForm/ImageUploadControls.js
+++ client/src/pages/AddItemForm/ImageUploadControls.js
@@ -7,25 +7,34 @@ import {
   IconButton,
   Image,
   ButtonGroup,
+  Spinner,
+  Center,
 } from "@chakra-ui/react";
 import { useState } from 'react';
 import FileUploadButton from '../../components/FileUploadButton';
 import { FaTrash } from 'react-icons/fa';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { ADD_TEMP_IMAGE, REMOVE_TEMP_IMAGE } from '../../utils/mutations';
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from '../../utils/firebase';
 import { generateFileName, validateImageType } from '../../utils/imageUploads';
+import { GET_TEMP_IMAGE_FILE } from '../../utils/queries';
 
 const DEFAULT_IMAGE_LOCATION = "/no_image_uploaded.png";
 
-const ImageUploadControls = ({userId}) => {
+const ImageUploadControls = ({ userId }) => {
   const [addTempImage, { loading: addTempImageLoading }] = useMutation(ADD_TEMP_IMAGE);
   const [removeTempImage, { loading: removeTempImageLoading }] = useMutation(REMOVE_TEMP_IMAGE);
   const [isUploading, setIsUploading] = useState(false);
   const [imageURL, setImageURL] = useState(DEFAULT_IMAGE_LOCATION);
+  const { loading: getTempImageLoading, data: getTempImageData } = useQuery(GET_TEMP_IMAGE_FILE, { variables: { userId } });
   const toast = useToast();
-  
+
+  if (getTempImageData?.user.tempImageFile && !(imageURL.includes(getTempImageData.user.tempImageFile))) {
+    getDownloadURL(ref(storage, getTempImageData.user.tempImageFile))
+      .then(url => setImageURL(url));
+  }
+
 
   const fileUploadHandler = (event) => {
     const file = event.target.files[0];
@@ -64,7 +73,7 @@ const ImageUploadControls = ({userId}) => {
 
   return (
     <Box>
-      <FormControl>
+      <FormControl mb={1}>
         <FormLabel htmlFor="fileUpload">Picture</FormLabel>
         <ButtonGroup>
           <FileUploadButton
@@ -78,7 +87,16 @@ const ImageUploadControls = ({userId}) => {
           <IconButton aria-label='Discard Image' isLoading={removeTempImageLoading} icon={<FaTrash />} isDisabled={imageURL === DEFAULT_IMAGE_LOCATION} onClick={discardImageHandler} />
         </ButtonGroup>
       </FormControl>
-      <Image maxW="320px" maxH="512px" border={1} boxShadow="md" src={imageURL} />
+      {
+        (isUploading || addTempImageLoading || removeTempImageLoading || getTempImageLoading)
+          ?
+          <Center w="320px" h="512px" border={1} boxShadow="md">
+            <Spinner size='xl' />
+          </Center>
+          :
+          <Image maxW="320px" maxH="512px" border={1} boxShadow="md" src={imageURL} />
+      }
+
     </Box>
   );
 
