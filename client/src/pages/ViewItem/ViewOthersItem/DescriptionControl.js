2cb18457d4c00b395f45f85873932ed9e51281aa
Brian D. Baker <brian.devs.baker@gmail.com>
2022-01-15 23:00:05 -0500

    Fixed ViewItem and ViewOthersItem

--- client/src/pages/ViewItem/ViewOthersItem/DescriptionControl.js
+++ client/src/pages/ViewItem/ViewOthersItem/DescriptionControl.js
@@ -2,19 +2,14 @@ import React from 'react'
 import {
   FormControl,
   FormLabel,
-  Input
+  Text
 } from "@chakra-ui/react";
 
 const DescriptionControl = ({ description }) => {
   return (
     <FormControl>
       <FormLabel htmlFor="description">Description</FormLabel>
-      <Input
-        id="description"
-        isDisabled={true}
-        value={description}
-        maxLength="50"
-      />
+      <Text>{description}</Text>
     </FormControl>
   );
 }
