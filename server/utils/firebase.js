b55b16c44dc313c0149dda069236e79c3708e0b5
Brian D. Baker <brian.devs.baker@gmail.com>
2022-01-17 14:14:33 -0500

    Debugged deployment issues

--- server/utils/firebase.js
+++ server/utils/firebase.js
@@ -1,7 +1,20 @@
 //const path = require('path');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS); //require(path.join('..', process.env.GOOGLE_APPLICATION_CREDENTIALS));
+
+require('dotenv').config();
+const serviceAccount = {
+  "type": process.env.GOOGLE_TYPE,
+  "project_id": process.env.GOOGLE_PROJECT_ID,
+  "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
+  "private_key": process.env.GOOGLE_PRIVATE_KEY,
+  "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+  "client_id": process.env.GOOGLE_CLIENT_ID,
+  "auth_uri": process.env.GOOGLE_AUTH_URI,
+  "token_uri": process.env.GOOGLE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.GOOGLE_CLIENT_X509_CERT_URL
+} 
 
 initializeApp({
   credential: cert(serviceAccount),
