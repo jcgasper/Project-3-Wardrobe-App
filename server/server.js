68804faea8b7cc49f06e197f580d7da045edcb1c
mdbow22 <mdbow22@gmail.com>
2022-01-18 01:06:46 -0500

    deleted an empty line

--- server/server.js
+++ server/server.js
@@ -27,7 +27,6 @@ server.start().then(() => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
-  
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
