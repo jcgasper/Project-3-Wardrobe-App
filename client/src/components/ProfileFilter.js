bca523e44549dd1e457b7143c1f5da206936e39b
mdbow22 <mdbow22@gmail.com>
2022-01-25 21:46:44 -0500

    restyled inputs and added category filter to profile page:

--- client/src/components/ProfileFilter.js
+++ client/src/components/ProfileFilter.js
@@ -10,8 +10,9 @@ import {
 } from "@chakra-ui/react";
 import { FaFilter } from 'react-icons/fa';
 import TagFilter from './TagFilter';
+import CategoryFilter from './CategoryFilter';
 
-function ProfileFilter({clothes, setCategories, setTags, tags}) {
+function ProfileFilter({clothes, categories, setCategories, setTags, tags}) {
 
     const [availableTags, setAvailableTags] = useState(getTags());
 
@@ -24,6 +25,20 @@ function ProfileFilter({clothes, setCategories, setTags, tags}) {
             return {value: tag, label: tag}
         });
     }
+
+    useEffect(() => {
+      if(!categories) {
+        setAvailableTags(getTags());
+      } else {
+        let allTags = [];
+        clothes.filter(article => article.category === categories)
+          .forEach(article => allTags = [...allTags, ...article.tags]);
+        
+        allTags = [...new Set(allTags)].sort();
+
+        setAvailableTags(allTags.map(tag => ({value: tag, label: tag})));
+      }
+    }, [categories]);
     
 
   return (
@@ -38,7 +53,8 @@ function ProfileFilter({clothes, setCategories, setTags, tags}) {
           </AccordionButton>
         </h2>
         <AccordionPanel pb={4}>
-          <TagFilter clothes={clothes} availableTags={availableTags} setAvailableTags={setAvailableTags} setTags={setTags} tags={tags} />
+          <CategoryFilter clothes={clothes} setAvailableTags={setAvailableTags} setCategories={setCategories} />
+          <TagFilter clothes={clothes} categories={categories} availableTags={availableTags} setAvailableTags={setAvailableTags} setTags={setTags} tags={tags} />
         </AccordionPanel>
       </AccordionItem>
     </Accordion>
